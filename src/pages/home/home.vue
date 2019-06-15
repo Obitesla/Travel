be017d4d3b4254b005c2f57fbfbae6ec580c98e1
unknown <obitesla@gmail.com>
2019-06-15 18:22:26 +0800

    projrct init

---
+++ src/pages/home/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>home</div>
+    <router-link to="to">123</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style scoped>
+
+</style>
