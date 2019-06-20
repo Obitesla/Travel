e2a007b3f8a3322509462f71b2a107559076a74f
unknown <obitesla@gmail.com>
2019-06-20 21:41:10 +0800

    city-router

--- src/pages/home/components/header.vue
+++ src/pages/home/components/header.vue
@@ -8,10 +8,12 @@
         <span class="iconfont">&#xe6a2;</span>
         输入城市/景点/游玩主题
       </div>
-      <div class="header-right">
-        {{this.city}}
-        <span class="iconfont arrow-icon">&#xe66c;</span>
-      </div>
+      <router-link to="/city">
+        <div class="header-right">
+          {{this.city}}
+          <span class="iconfont arrow-icon">&#xe66c;</span>
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
@@ -29,7 +31,7 @@ export default {
   @import '~styles/varibles.styl'
   .header
     display:flex
-    line-height: .86rem
+    line-height: $headerHeight
     background $bgColor //dbColor为所设置的全局的一个背景色，在‘@/assets/styles/varibles.styl’中更改
     color #ffffff
     .header-left
@@ -52,6 +54,7 @@ export default {
       width: 1.24rem
       float: right
       text-align center
+      color #ffffff
       .arrow-icon
         margin-left -0.04rem
         font-size .24rem
