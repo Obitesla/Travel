1f0b110b8de798818a5ce76a0a35435314d2fbd1
unknown <obitesla@gmail.com>
2019-07-18 11:03:42 +0800

    banner finish

--- src/pages/home/components/recommend.vue
+++ src/pages/home/components/recommend.vue
@@ -2,14 +2,15 @@
   <div>
     <div class="recommmend-title">热销推荐</div>
     <ul>
-      <li v-for="item of list" class="item border-bottom" :key="item.id">
-        <img class="item-img" :src="item.imgUrl"/>
-        <div class="item-info">
-          <p class="item-title">{{item.title}}</p>
-          <p class="item-desc">{{item.desc}}</p>
-          <button class="item-button">查看详情</button>
-        </div>
-      </li>
+        <router-link v-for="item of list" class="item border-bottom" :key="item.id" :to="'/detail/' + item.id" tag="li">
+          <img class="item-img" :src="item.imgUrl"/>
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+            <button class="item-button">查看详情</button>
+          </div>
+        </router-link>
+
     </ul>
   </div>
 </template>
